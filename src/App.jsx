cc4bdf802e9c3d1cfb5cd5f88f64b0eb53ee2cba
TonyAchuJr <achuaswinvigneshr@gmail.com>
2026-06-18 18:49:31 +0530

    Change file input display style from 'none' to 'block'

--- src/App.jsx
+++ src/App.jsx
@@ -662,7 +662,7 @@ function ProjCard({ proj, profiles, onClick, isOwner, onArchive, onDelete, authU
   id={`cover-${proj.id}`}
   type="file"
   accept="image/*"
-  style={{ display: "none" }}
+  style={{ display: "block" }}
   onChange={async (e) => {
     const file = e.target.files?.[0];
     if (!file) return;
