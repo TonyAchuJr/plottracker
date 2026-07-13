804bdb86820ad822026ef6816074401049a7a3cb
TonyAchuJr <achuaswinvigneshr@gmail.com>
2026-07-13 13:34:03 +0530

    Update LayoutView.jsx

--- src/components/LayoutView.jsx
+++ src/components/LayoutView.jsx
@@ -1,37 +1,85 @@
-export default function LayoutView({ proj }) {
-
-  const layoutImage = proj.cover_image;
-
-  if (!layoutImage) {
+export default function LayoutView({ proj, plots, layoutCoords }) {
+  if (!proj?.layout_image) {
     return (
       <div className="layout-empty">
-
-        <h3>No Layout Image</h3>
-
-        <p>
-          Upload a Master Layout Image from Project Settings.
-        </p>
-
+        <h3>No Master Layout Uploaded</h3>
+        <p>Upload a layout image in Project Settings.</p>
       </div>
     );
   }
 
   return (
-
-    <div className="layout-view">
-
-      <img
-    src={layoutImage}
-    alt="Project Layout"
-    style={{
+    <div
+      style={{
+        position: "relative",
         width: "100%",
-        borderRadius: "18px",
-        display: "block"
-    }}
-/>
+        marginTop: 20
+      }}
+    >
+      <img
+        src={proj.layout_image}
+        alt="Master Layout"
+        style={{
+          width: "100%",
+          display: "block",
+          borderRadius: 18
+        }}
+      />
 
+      {layoutCoords.map(coord => {
+
+        const plot = plots.find(
+          p => Number(p.number) === Number(coord.plot_number)
+        );
+
+        let color = "#22c55e";
+
+        if (plot?.status === "booked")
+          color = "#f59e0b";
+
+        if (plot?.status === "sold")
+          color = "#ef4444";
+
+        return (
+          <div
+            key={coord.id}
+            style={{
+              position: "absolute",
+
+              left: coord.x,
+
+              top: coord.y,
+
+              width: coord.width,
+
+              height: coord.height,
+
+              background: color,
+
+              opacity: .45,
+
+              border: `2px solid ${color}`,
+
+              borderRadius: 8,
+
+              color: "white",
+
+              display: "flex",
+
+              justifyContent: "center",
+
+              alignItems: "center",
+
+              fontWeight: "bold",
+
+              cursor: "pointer"
+            }}
+          >
+            {coord.plot_number}
+          </div>
+        );
+
+      })}
     </div>
-
   );
-
 }
